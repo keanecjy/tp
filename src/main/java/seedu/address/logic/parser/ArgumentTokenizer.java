a920e4e43c535c56a8926c5ca8a7910c8d5a252f
keanecjy <e0425601@u.nus.edu>
2020-09-17 21:40:55 +0800

    Update storage

--- src/main/java/seedu/address/logic/parser/ArgumentTokenizer.java
+++ src/main/java/seedu/address/logic/parser/ArgumentTokenizer.java
@@ -114,8 +114,7 @@ public class ArgumentTokenizer {
      * The end position of the value is determined by {@code nextPrefixPosition}.
      */
     private static String extractArgumentValue(String argsString,
-                                        PrefixPosition currentPrefixPosition,
-                                        PrefixPosition nextPrefixPosition) {
+            PrefixPosition currentPrefixPosition, PrefixPosition nextPrefixPosition) {
         Prefix prefix = currentPrefixPosition.getPrefix();
 
         int valueStartPos = currentPrefixPosition.getStartPosition() + prefix.getPrefix().length();
